8dfb87c179efa20f18f4e630232f81cc4fe27dad
Florent <fdevillard@me.com>
2019-03-05 16:00:03 +0100

    Benchmarked `TimeSeries.merge`
    GitOrigin-RevId: 3af39935681652a5a8a012c89592cea5c10b8aa0

---
+++ src/test/scala/io/sqooba/timeseries/TimeSeriesBenchmark.scala
@@ -0,0 +1,49 @@
+package io.sqooba.timeseries
+
+import java.lang.Math.floorMod
+
+import io.sqooba.timeseries.immutable.{TSEntry, VectorTimeSeries}
+import org.scalameter.api._
+
+import scala.util.Random
+
+object TimeSeriesBenchmark extends Bench.ForkedTime {
+
+  val random = Random
+
+  def randomEntryWithTimestamp(timestamp: Long): TSEntry[Int] =
+    TSEntry(timestamp, random.nextInt(30), floorMod(random.nextLong(), 10L) + 1)
+
+  def createTimeSeriesWithSize(size: Int): TimeSeries[Int] = {
+    val entries = new Array[TSEntry[Int]](size)
+    entries(0) = randomEntryWithTimestamp(0)
+
+    for (i <- 1 until size) {
+      val newTimestamp = entries(i - 1).definedUntil + floorMod(random.nextLong(), 10)
+      entries(i) = randomEntryWithTimestamp(newTimestamp)
+    }
+
+    new VectorTimeSeries(entries.toVector)
+  }
+
+  val sizes = Gen.range("size")(100000, 1000000, 100000)
+
+  val ts = for {
+    size <- sizes
+  } yield (createTimeSeriesWithSize(size), createTimeSeriesWithSize(size))
+
+  performance of "TimeSeries" config (
+    exec.independentSamples -> 4,
+    exec.benchRuns          -> 4
+  ) in {
+    measure method "merge" in {
+      using(ts) in {
+        case (ts1, ts2) =>
+          ts1.merge[Int, Int] {
+            case (Some(v), _) => Some(v)
+            case _            => None
+          }(ts2)
+      }
+    }
+  }
+}
