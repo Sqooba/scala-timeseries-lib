a11571bd623d86aa35a21116dd6821d06897b9f2
Yann Bolliger <yann.bolliger@epfl.ch>
2019-08-12 13:41:36 +0200

    Add wrapper for gorilla encoding library.
    GitOrigin-RevId: 66ae6a346f2c8527d8c6e5da8adf7fccf612c605

---
+++ src/test/scala/io/sqooba/timeseries/archive/TSCompressionSpec.scala
@@ -0,0 +1,56 @@
+package io.sqooba.timeseries.archive
+
+import io.sqooba.timeseries.TimeSeries
+import io.sqooba.timeseries.immutable.TSEntry
+import org.scalatest.{FlatSpec, Matchers}
+
+class TSCompressionSpec extends FlatSpec with Matchers {
+
+  val tsDouble = TimeSeries(
+    Seq(
+      TSEntry(1, 200.03, 100),
+      TSEntry(50, 400.03, 100),
+      TSEntry(77, 100.03, 100),
+      TSEntry(200, 0.123456789, 100)
+    )
+  )
+
+  val tsLong = TimeSeries(
+    Seq(
+      TSEntry(1, 1L, 100),
+      TSEntry(50, Long.MaxValue, 100),
+      TSEntry(77, Long.MinValue, 100),
+      TSEntry(200, 123456789L, 100),
+      TSEntry(1234123234L, 87767666566L, 100)
+    )
+  )
+
+  "TSCompression" should "compress and again decompress a timeseries" in {
+    val (byteArray1, byteArray2) = TSCompression.compress(tsDouble.entries.toStream)
+    TSCompression.decompress(byteArray1, byteArray2) shouldBe tsDouble.entries
+  }
+
+  it should "compress and again decompress for converted long values" in {
+    val (byteArray1, byteArray2) = TSCompression.compress(tsLong.map(_.toDouble).entries.toStream)
+
+    TSCompression.decompress(byteArray1, byteArray2) shouldBe tsLong.entries
+  }
+
+  it should "throw if the byte arrays are not of same length" in {
+    val (byteArrayV, byteArrayD) = TSCompression.compress(tsDouble.entries.toStream)
+
+    an[IllegalArgumentException] should be thrownBy
+      TSCompression.decompress(
+        byteArrayV.drop(8),
+        byteArrayD
+      )
+  }
+
+  it should "throw if an empty stream is provided to compress" in {
+    an[IllegalArgumentException] should be thrownBy TSCompression.compress(Stream.empty)
+  }
+
+  it should "return an empty stream for empty byte arrays" in {
+    an[IllegalArgumentException] should be thrownBy TSCompression.decompress(Array.empty, Array.empty)
+  }
+}
