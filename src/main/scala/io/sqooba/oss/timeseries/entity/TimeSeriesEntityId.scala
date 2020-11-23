90e292b21d401336d48dd6b5df27eb219a2c4597
Yann Bolliger <yann.bolliger@epfl.ch>
2020-11-23 12:56:11 +0100

    Free entity id of the long type requirement.
    GitOrigin-RevId: 4669495f7a7eb089ecd80a86a0f9dbfa7e0ddc32

--- src/main/scala/io/sqooba/oss/timeseries/entity/TimeSeriesEntityId.scala
+++ src/main/scala/io/sqooba/oss/timeseries/entity/TimeSeriesEntityId.scala
@@ -1,17 +1,22 @@
 package io.sqooba.oss.timeseries.entity
 
+/** Identifies an entity that can have time series data attached to it. The different
+ * possible signals are defined by [[io.sqooba.oss.timeseries.entity.TsLabel!]]s.
+ *
+ * @note It is the implementor's responsibility to choose a way of identifying
+ * entities. Be that by different type, numerical or lexical identifiers. The idea is
+ * that users define the identifying attributes on their implementation of the trait.
+ */
+// Not requiring a specific type and shape of identifying property leaves the choice
+// to the user and also enables use-cases where multiple identifying mechanisms need
+// to be combined with trait polymorphism.
 trait TimeSeriesEntityId {
 
   /**
-    * @return the internal identification number of whatever the implementor identifies.
-    */
-  def id: Long
-
-  /**
-    * @param signal the label of the time series signal
-    * @return a TsId that fully identifies a time series signal of a the entity
-    *         specified by this EntityId
-    */
+   * @param signal the label of the time series signal
+   * @return a TsId that fully identifies a time series signal of a the entity
+   *         specified by this EntityId
+   */
   // TODO: the use of this function probably causes AnyVal implementations to require an
   //   instantiation. At the moment we mostly care about correctness, not performance, but
   //   we need to keep it in mind. If at some point we want to do things allocation-free,
